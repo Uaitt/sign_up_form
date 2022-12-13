5b54b6dac9c3c17224fa525df6c718a85c9ded40
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-12-13 15:20:19 +0100

    Add check for password pattern

--- scripts/script.js
+++ scripts/script.js
@@ -11,30 +11,35 @@ function customizeElement(input) {
   if(input.value == '') {
     defaultStyling(input);
   }
-  else if (input.validity.valid) {
+  else if (input.validity.valid && input.getAttribute('type') != 'password' || passwordValid(input)) {
     validStyling(input);
   }
   else {
-    invalidStyling(input)
+    invalidStyling(input);
   }
 }
 
 function defaultStyling(input) {
   input.style.borderColor = 'white white rgb(196, 194, 191)';
-  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  let span = document.querySelector(`span#${input.getAttribute('id')}`);
   span.innerHTML = '';
 }
 
 function validStyling(input) {
   input.style.borderColor = 'white white rgb(80, 192, 80)';
-  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  let span = document.querySelector(`span#${input.getAttribute('id')}`);
   span.innerHTML = '✓';
   span.style.color = 'rgb(80, 192, 80)';
 }
 
 function invalidStyling(input) {
   input.style.borderColor = 'white white rgb(216, 107, 107)';
-  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  let span = document.querySelector(`span#${input.getAttribute('id')}`);
   span.innerHTML = '✖';
   span.style.color = 'rgb(216, 107, 107)';
+}
+
+function passwordValid(input) {
+  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/
+  return input.getAttribute('name') == 'user_password' && passwordPattern.test(input.value)
 }
