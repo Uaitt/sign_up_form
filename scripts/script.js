6b6e633a1f02588f8391a66ee449d9e221c021f5
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-12-13 14:56:13 +0100

    Refactor

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,32 @@ document.querySelector('form').addEventListener('submit', (event) => {
 
 function customizeElement(input) {
   if(input.value == '') {
-    input.style.borderColor = 'white white rgb(196, 194, 191)'
-    span = document.querySelector(`span#${input.getAttribute('id')}`)
-    span.innerHTML = ""
+    defaultStyling(input);
   }
   else if (input.validity.valid) {
-    input.style.borderColor = 'white white rgb(80, 192, 80)'
-    span = document.querySelector(`span#${input.getAttribute('id')}`)
-    span.innerHTML = "✓"
-    span.style.color = "rgb(80, 192, 80)"
+    validStyling(input);
   }
   else {
-    input.style.borderColor = 'white white rgb(216, 107, 107)'
-    span = document.querySelector(`span#${input.getAttribute('id')}`)
-    span.innerHTML = "✖"
-    span.style.color = "rgb(216, 107, 107)"
+    invalidStyling(input)
   }
+}
+
+function defaultStyling(input) {
+  input.style.borderColor = 'white white rgb(196, 194, 191)';
+  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  span.innerHTML = '';
+}
+
+function validStyling(input) {
+  input.style.borderColor = 'white white rgb(80, 192, 80)';
+  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  span.innerHTML = '✓';
+  span.style.color = 'rgb(80, 192, 80)';
+}
+
+function invalidStyling(input) {
+  input.style.borderColor = 'white white rgb(216, 107, 107)';
+  span = document.querySelector(`span#${input.getAttribute('id')}`);
+  span.innerHTML = '✖';
+  span.style.color = 'rgb(216, 107, 107)';
 }
