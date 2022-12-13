bb3c21d6e3262b12e6a5d7dcd369b02b9093cf84
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-12-13 11:42:58 +0100

    Reverse conditional order

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,16 @@ inputs.forEach( input => {
 });
 
 function checkValidity(input) {
-  if(input.validity.valid) {
+  if(input.value == '') {
+    input.style.borderColor = 'white white rgb(196, 194, 191)'
+    span = document.querySelector(`span#${input.getAttribute('id')}`)
+    span.innerHTML = ""
+  }
+  else if (input.validity.valid) {
     input.style.borderColor = 'white white rgb(80, 192, 80)'
     span = document.querySelector(`span#${input.getAttribute('id')}`)
     span.innerHTML = "✓"
     span.style.color = "rgb(80, 192, 80)"
-  }
-  else if (input.value == '') {
-      input.style.borderColor = 'white white rgb(196, 194, 191)'
-      span = document.querySelector(`span#${input.getAttribute('id')}`)
-      span.innerHTML = ""
     }
   else {
       input.style.borderColor = 'white white rgb(216, 107, 107)'
