ace20622c78fce82b52964524e33c2bb5b3e874b
Lorenzo Zabot <lorenzozabot@gmail.com>
2022-12-13 10:53:43 +0100

    Add dynamic color highlight

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 const inputs = document.querySelectorAll('input')
 
-/*
+
 inputs.forEach( input => {
-  input.addEventListener('input', checkValidity(input))
+  input.addEventListener('focusout', checkValidity.bind(this, input))
 });
-*/
+
+function checkValidity(input) {
+  if(input.validity.valid) {
+    input.style.borderColor = 'white white green'
+    span = document.querySelector('span#name')
+    span.innerHTML += "h"
+  }
+  else {
+    input.style.borderColor = 'white white red'
+  }
+}
